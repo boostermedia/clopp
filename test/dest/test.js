4270f55509a49664d5cbedf84f1040dce5cbda9e
Riko Ophorst <riko.ophorst@boostermedia.com>
2014-12-15 17:18:44 +0100

    - Fix bugs & html support

--- test/dest/test.js
+++ test/dest/test.js
@@ -1,3 +1,3 @@
 
 
-nee!
+		jaaaa!
--- test/src/test.js
+++ test/src/test.js
@@ -1,10 +1,16 @@
 /* #define environment (function(){console.log();}) */
 
 /* #if 1+1==3 */
-ja!
-// #elseif "lol"=="ll"
-jaaaa!
-// #endelseif
-/* #else */
-nee!
+	ja!
+	/* #elseif "lol"=="l0l" */
+		jaaaa!fdg
+	/* #endelseif */
+	/* #elseif "lol"=="lol" */
+		jaaaa!
+	/* #endelseif */
+	/* #elseif 5+5==10
+		david holy balls t werkt gewoon!
+	/* #endelseif */
+	/* #else */
+		nee!
 /* #endif */
